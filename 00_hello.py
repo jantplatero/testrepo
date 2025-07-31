6ef8da511fa5317b92c113b6bbc953db511617b2
jantplatero <jantplatero@hotmail.com>
2025-07-31 10:44:59 +0200

    Add files via upload

---
+++ 00_hello.py
@@ -0,0 +1,28 @@
+# Comentario: Hola mundo
+# Otro más
+print("Hola Python")
+print('Hola Python')
+
+
+
+# Tipos de datos
+# type consulta el tipo de dato
+print(type('Hola Python')) #Tipo 'str'
+print(type(5)) #Tipo 'int'
+print(type(1.7)) #Tipo 'float'
+print(type(3 + 1j)) #Tipo 'complex'
+print(type(True)) #Tipo 'bool'
+
+
+"""
+Comentario
+en 
+varias lineas
+"""
+
+'''
+Más comentarios
+en varias líneas
+'''
+
+
